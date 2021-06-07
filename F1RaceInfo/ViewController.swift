013f49da81ac6300f5368c15bf161d39fd2bd066
LightRid <lightrid@gmail.com>
2021-06-07 23:26:36 +0300

    Initial Commit

---
+++ F1RaceInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1RaceInfo
+//
+//  Created by Mykhailo Kviatkovskyi on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
