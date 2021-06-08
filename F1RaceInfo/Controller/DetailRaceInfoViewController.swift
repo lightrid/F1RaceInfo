e0c5b54ecb724c56e164c7cc6f7fc0689ae3bae9
LightRid <lightrid@gmail.com>
2021-06-08 23:46:14 +0300

    UX creation

---
+++ F1RaceInfo/Controller/DetailRaceInfoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailRaceInfoViewController.swift
+//  F1RaceInfo
+//
+//  Created by Mykhailo Kviatkovskyi on 08.06.2021.
+//
+
+import UIKit
+
+class DetailRaceInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
